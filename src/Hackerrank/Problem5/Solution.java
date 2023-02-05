67247cadd613b5536de4bf08d8f18ac86f741f40
parasharparth <parthparashar6@gmail.com>
2023-02-04 23:16:51 -0800

    Solution-5-diagonal-difference

---
+++ src/Hackerrank/Problem5/Solution.java
@@ -0,0 +1,11 @@
+package Hackerrank.Problem5;
+
+public class Solution {
+	public static void main(String[] args){
+		int[][] arr = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+		int sum_primary = 0;
+		int sum_secondary = 0;
+
+
+	}
+}
